1650159ce55c64000e2055c62a7ffc88ce1f771c
Sapna Rathod <72684602+spna56@users.noreply.github.com>
2021-01-30 14:42:36 +0530

    Delete key.js

--- config/key.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-    mongoURI:'mongodb+srv://sapna:sapna@cluster0-8e01b.mongodb.net/test?retryWrites=true&w=majority',
-    googleClientID:'1046065176460-phr646nicod2hot46m52ti3hls2e4nah.apps.googleusercontent.com',
-    googleClientSecret:'HWGehpFStnq9s73USCloAL9a'
-}
